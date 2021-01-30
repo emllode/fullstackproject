217849b872939e6d4051530c2134891c29960a02
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-01-30 13:07:55 +0100

    started working on getting postgreSQL working.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("hello world");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+
+
+
+ const main = async () => {
+
+    const orm = await MikroORM.init({
+        entities: [],
+        dbName: 'fullstack',
+        user: '',
+        password: '',
+        type: 'postgresql',
+        debug: !__prod__,
+    });
+
+ }
+
+ main()
+
+
+
